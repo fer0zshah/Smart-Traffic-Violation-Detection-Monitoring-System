6d2f4edc2f8c9951c385d6ba014757044e88574f
Md Feroz Shah <fizzz6668@gmail.com>
2026-07-19 20:28:14 +0600

    STTI-121 done

--- epic6/app/resources/views/admin/index.blade.php
+++ epic6/app/resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    Administrator access verified. User and system controls will be added here.
+                    Administrator access verified. User and system controls will be added here later.
                 </div>
             </div>
         </div>
